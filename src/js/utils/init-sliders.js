fead7fb58c7b907c7c583ec59e80048fcfe5eb7f
Vadim Tiuriumin <owlriks@gmail.com>
2022-05-19 15:35:40 +0800

    Стилизована главная страница

---
+++ src/js/utils/init-sliders.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+
+  $(document).ready(function () {
+    if ($('.swiper').length) {
+      const testimonialsSlider = new Swiper('.testimonials-slider .swiper', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+
+        navigation: {
+          nextEl: '.testimonials-slider .slider-controls__btn.--next',
+          prevEl: '.testimonials-slider .slider-controls__btn.--prev',
+        },
+        breakpoints: {
+          1024: {
+            slidesPerView: 2,
+          },
+          1440: {
+            slidesPerView: 2.5,
+            spaceBetween: 30,
+          }
+        }
+      });
+    }
+  });
+})();
